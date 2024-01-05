a81fabba5bd3d97036ecd601a071d85b789a7e56
OKOKO Godwin <godwinjnrokokoorokpo@gmail.com>
2024-01-05 12:59:05 +0100

    update vandetail page styling

--- src/Pages/VanDetail.js
+++ src/Pages/VanDetail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, useLocation } from 'react-router'
 import { Link } from 'react-router-dom'
+import './vanDetail.css'
 
 function VanDetail() {
     const [van, setVan] = useState(null)
@@ -20,13 +21,15 @@ function VanDetail() {
 
       {
         van ? (<div className='van-detail'>
-          <Link to={`..?${searchTerm}`} relative='path'>&larr; {`Back to ${query} Vans`}</Link>
-            <img src={van.imageUrl} alt={`image of ${van.name} van`}/>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
-            <h2>{van.name}</h2>
-            <p><span>${van.price}</span>/day</p>
-            <p>{van.description}</p>
-            <button className='link-button'>Rent this van</button>
+                  <Link to={`..?${searchTerm}`} relative='path'>&larr; {`Back to ${query} Vans`}</Link>
+                    <img src={van.imageUrl} alt={`image of ${van.name} van`}/>
+                   <div className='vandetail-content'>
+                      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                      <h2 className='van-name'>{van.name}</h2>
+                      <p><span className='price'>${van.price}</span>/day</p>
+                      <p className='van-description'>{van.description}</p>
+                  </div>
+                  <div className='rent-van-btn' ><Link className='rent-van-link' >Rent this van</Link></div>
         </div>): 'Loading...'
       }
     </div>
